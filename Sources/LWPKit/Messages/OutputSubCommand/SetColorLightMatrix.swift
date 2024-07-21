9318a68edebabfcc9da04f5814b38a06ef04cf82
Shinichiro Oba <bricklife@gmail.com>
2024-07-22 00:03:43 +0900

    Change SetColorLightMatrix interface

--- Sources/LWPKit/Messages/OutputSubCommand/SetColorLightMatrix.swift
+++ Sources/LWPKit/Messages/OutputSubCommand/SetColorLightMatrix.swift
@@ -6,22 +6,22 @@ public struct SetColorLightMatrix: WriteDirectModeData {
 
     public let portID: Port.ID
     public let startupAndCompletionInformation: UInt8
-    public let colorIDs: [Color]
-    public let colorLevels: [UInt8]
+    public let colorMatrix: [Color]
+    public let brightnessMatrix: [UInt8]
 
-    public static let defaultColorLevels: [UInt8] = .init(repeating: 0x0a, count: 9)
+    public static let defaultBrightnessMatrix: [UInt8] = .init(repeating: 10, count: 9)
 
-    public init(portID: Port.ID, startupAndCompletionInformation: UInt8 = 0x11, colorIDs: [Color], colorLevels: [UInt8] = defaultColorLevels) {
+    public init(portID: Port.ID, startupAndCompletionInformation: UInt8 = 0x11, colorMatrix: [Color], brightnessMatrix: [UInt8] = defaultBrightnessMatrix) {
         self.portID = portID
         self.startupAndCompletionInformation = startupAndCompletionInformation
-        self.colorIDs = colorIDs
-        self.colorLevels = colorLevels
+        self.colorMatrix = colorMatrix
+        self.brightnessMatrix = brightnessMatrix
     }
 
     public func writeDirectModeDataPayload() throws -> [UInt8] {
-        let array = Array(zip(colorIDs, colorLevels))
+        let array = Array(zip(colorMatrix, brightnessMatrix))
         guard array.count == 9 else { throw SetColorLightMatrix.Error.numOfColorError }
-        return array.map { (id, level) in (level << 4) | (id.rawValue & 0x0f) }
+        return array.map { (color, brightness) in (brightness << 4) | (color.rawValue & 0x0f) }
     }
 
     public static func canUse(for ioType: IOType) -> Bool {
